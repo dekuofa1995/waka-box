55df29f07f131db4fb2805cbd212755ab6976686
dekuofa1995 <dekuofa1995@gmail.com>
2020-06-08 16:51:22 +0800

    change api use summary

--- index.js
+++ index.js
@@ -1,80 +1,99 @@
-require("dotenv").config();
-const { WakaTimeClient, RANGE } = require("wakatime-client");
-const Octokit = require("@octokit/rest");
+require('dotenv').config()
+const { WakaTimeClient, RANGE } = require('wakatime-client')
+const Octokit = require('@octokit/rest')
 
 const {
   GIST_ID: gistId,
   GH_TOKEN: githubToken,
-  WAKATIME_API_KEY: wakatimeApiKey
-} = process.env;
+  WAKATIME_API_KEY: wakatimeApiKey,
+} = process.env
+const wakatime = new WakaTimeClient(wakatimeApiKey)
 
-const wakatime = new WakaTimeClient(wakatimeApiKey);
+const octokit = new Octokit({ auth: `token ${githubToken}` })
 
-const octokit = new Octokit({ auth: `token ${githubToken}` });
-
+function weekBefore() {
+  const date = new Date()
+  date.setDate(date.getDate() - 7)
+  return date
+}
 async function main() {
-  const stats = await wakatime.getMyStats({ range: RANGE.LAST_7_DAYS });
-  console.log("stats: ", stats)
-  await updateGist(stats);
+  try {
+    const stats = await wakatime.getMySummary({
+      dateRange: {
+        startDate: weekBefore(),
+        endDate: new Date(),
+      }
+    })
+    await updateGist(stats)
+  } catch (e) {
+    console.error(e)
+  }
+}
+function formatSeconds (seconds) {
+  return new Date(seconds * 1000).toISOString().substr(11, 8)
 }
 
 async function updateGist(stats) {
-  let gist;
+  let gist
   try {
-    gist = await octokit.gists.get({ gist_id: gistId });
+    gist = await octokit.gists.get({ gist_id: gistId })
   } catch (error) {
-    console.error(`Unable to get gist\n${error}`);
+    console.error(`Unable to get gist\n${error}`)
   }
-
-  const lines = [];
-  for (let i = 0; i < Math.min(stats.data.languages.length, 5); i++) {
-    const data = stats.data.languages[i];
-    const { name, percent, text: time } = data;
-
-    const line = [
-      name.padEnd(11),
-      time.padEnd(14),
-      generateBarChart(percent, 21),
-      String(percent.toFixed(1)).padStart(5) + "%"
-    ];
-
-    lines.push(line.join(" "));
+  const arr = stats.data.map(({ languages }) => languages).flat()
+  const sum = arr.reduce((acc, curr) => acc + curr.total_seconds, 0)
+  const converted = Array.from(arr.reduce((acc, curr) => {
+    const accItem = acc.get(curr.name) 
+    if (accItem) {
+      const total = accItem.total_seconds + curr.total_seconds
+      acc.set(curr.name, { ...accItem, total_seconds: total, percent: total / sum })
+    } else {acc.set(curr.name, curr)}
+    return acc
+  }, new Map()).values())
+  const lines = converted.map(({ name, percent, total_seconds }) => [
+    name.padEnd(11),
+    generateBarChart(percent, 21),
+    String(percent.toFixed(1)).padStart(5) + '%',
+    formatSeconds(total_seconds).padEnd(14),
+  ].join(' '))
+  if (lines.length == 0) {
+    console.warn('no data to update')
+    return
   }
-
-  if (lines.length == 0) return;
-
+  const content = lines.join('\n')
   try {
     // Get original filename to update that same file
-    const filename = Object.keys(gist.data.files)[0];
+    const filename = Object.keys(gist.data.files)[0]
     await octokit.gists.update({
       gist_id: gistId,
       files: {
         [filename]: {
           filename: `📊 Weekly development breakdown`,
-          content: lines.join("\n")
-        }
-      }
-    });
+          content: content,
+        },
+      },
+    })
+    console.log('update content: ', content)
   } catch (error) {
-    console.error(`Unable to update gist\n${error}`);
+    console.error(`Unable to update gist\n${error}`)
   }
 }
 
 function generateBarChart(percent, size) {
-  const syms = "░▏▎▍▌▋▊▉█";
+  const syms = '░▏▎▍▌▋▊▉█'
 
-  const frac = Math.floor((size * 8 * percent) / 100);
-  const barsFull = Math.floor(frac / 8);
+  const frac = Math.floor((size * 8 * percent) / 100)
+  const barsFull = Math.floor(frac / 8)
   if (barsFull >= size) {
-    return syms.substring(8, 9).repeat(size);
+    return syms.substring(8, 9).repeat(size)
   }
-  const semi = frac % 8;
+  const semi = frac % 8
 
   return [syms.substring(8, 9).repeat(barsFull), syms.substring(semi, semi + 1)]
-    .join("")
-    .padEnd(size, syms.substring(0, 1));
+    .join('')
+    .padEnd(size, syms.substring(0, 1))
 }
 
-(async () => {
-  await main();
-})();
+;(async () => {
+  await main()
+})()
